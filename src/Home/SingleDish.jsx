d7416b2d96bbd990ee13c67403261316808cf64c
Pratham Barua Nabo <nabobarua2@gmail.com>
2024-05-24 00:19:34 +0600

    dishes portion implemented

---
+++ src/Home/SingleDish.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { AiFillPlusCircle } from "react-icons/ai";
+import { assets } from "../assets/images/assets";
+
+const SingleDish = ({ food }) => {
+  const [count,setCount] = useState(0)
+  console.log(count)
+  return (
+    <div className="card bg-base-100 shadow-xl rounded-2xl">
+      <figure className="relative">
+        <img src={food.image} alt="Shoes" className="w-full rounded-2xl" />
+
+        <button onClick={()=>{setCount(count+1)}} className="absolute bottom-10 right-10 flex justify-center items-center gap-3">
+          <AiFillPlusCircle size={42} className="text-white font-semibold" />
+          <p className="text-white text-xl font-medium">{count}</p>
+        </button>
+      </figure>
+      <div className="card-body text-left">
+        <div className="flex justify-between">
+          <h2 className="card-title">{food.name}</h2>
+          <div  className="flex items-center">
+            <img sizes={23} src={assets.rating_starts} alt="" />
+          </div>
+        </div>
+        <p className="">{food.description}</p>
+        <p className="text-amber-500 text-2xl font-semibold">${food.price}</p>
+      </div>
+    </div>
+  );
+};
+
+export default SingleDish;
--- src/Home/TopDishes.jsx
+++ src/Home/TopDishes.jsx
@@ -1,11 +1,18 @@
-import React from 'react';
+import React from "react";
+import { food_list } from "../assets/images/assets";
+import SingleDish from "./SingleDish";
 
 const TopDishes = () => {
-    return (
-        <div className='text-center'>
-            <h1 className="text-xl md:text-2xl font-semibold">Top Dishes Near You</h1>
-        </div>
-    );
+  return (
+    <div className="text-center">
+      <h1 className="text-xl md:text-2xl font-semibold">Top Dishes Near You</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mt-5">
+        {food_list.map((food, index) => (
+          <SingleDish key={index} food={food}></SingleDish>
+        ))}
+      </div>
+    </div>
+  );
 };
 
 export default TopDishes;
